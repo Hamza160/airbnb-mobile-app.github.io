aecff5c7267164b0c93718958bfb5abce4431633
Hamza Ashraf <hamzaashraf160@gmail.com>
2024-01-24 04:57:44 +0500

    Search Destination Screen Added

--- src/screens/DestinationSearch/index.tsx
+++ src/screens/DestinationSearch/index.tsx
@@ -1,19 +1,34 @@
 import * as React from 'react';
-import {Text, TextInput, View} from 'react-native';
+import {FlatList, Text, TextInput, View} from 'react-native';
 import styles from './styles';
-
+import searchResults from '../../../assets/data/search';
+import Entypo from 'react-native-vector-icons/Entypo';
 const DestinationSearchScreen = () => {
   const [search, setSearch] = React.useState<string>('');
   return (
-    <View>
+    <View style={styles.container}>
       {/* Input Component */}
       <TextInput
         style={styles.searchInput}
         placeholder="Where are you going"
         value={search}
-        onChange={setSearch}
+        onChangeText={setSearch}
       />
       {/* List of Destination */}
+      <View>
+        <FlatList 
+          data={searchResults}
+          renderItem={({item}) => 
+          <View style={styles.row}>
+            <View style={styles.iconContainer}>
+              <Entypo name='location-pin' size={30} color={'#000'}/>
+            </View>
+            <Text style={styles.locationText}>{item.description}</Text>
+          </View>
+          }
+          keyExtractor={(item) => item.id}
+        />
+      </View>
     </View>
   );
 };
--- src/screens/DestinationSearch/styles.ts
+++ src/screens/DestinationSearch/styles.ts
@@ -1,7 +1,29 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
-  searchInput: {},
+  container: {
+    margin: 20,
+  },
+  searchInput: {
+    fontSize: 18,
+    marginBottom: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingVertical: 15,
+    borderBottomWidth: 1,
+    borderColor: 'lightgrey',
+  },
+  iconContainer: {
+    backgroundColor: '#E7E7E7',
+    padding: 7,
+    borderRadius: 10,
+    marginRight: 5,
+  },
+  locationText: {
+    color: '#000',
+  },
 });
 
 export default styles;
